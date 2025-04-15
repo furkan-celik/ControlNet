3898773f18d72001e4c9c3a0dfdaa804e239700f
ge32woz <fcelik@sabanciuniv.edu>
2025-04-15 18:07:16 +0200

    support validation img logging

--- cldm/logger.py
+++ cldm/logger.py
@@ -76,3 +76,7 @@ class ImageLogger(Callback):
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         if not self.disabled:
             self.log_img(pl_module, batch, batch_idx, split="train")
+    
+    def on_val_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        if not self.disabled:
+            self.log_img(pl_module, batch, batch_idx, split="val")
